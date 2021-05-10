6388f686dfb22c4eadf03901e2b9ddd53d8fb834
Daanish Raj <daanishraj@gmail.com>
2021-05-10 16:51:08 +0200

    Remove logo and delete img folder and svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 import React from "react"
-import logo from '../imgs/personio_logo.svg'
 
 const Header = ()=>{
     return (
         <div className="container">
-             <img src={logo} alt="Logo" />
             <h1>Applications List</h1>
         </div>
     )
